9f96d8a169b9e6e00ed61b0b042424045015de8d
Jigyansa <71638450+jigyansa018@users.noreply.github.com>
2025-09-09 23:52:06 +0530

    Add files via upload

---
+++ chap_6/prac_5.py
@@ -0,0 +1,10 @@
+l = ["Isha" ,"Harry" , "Chiku" , "Nishu" , "Omm"]
+
+name = input("Enter your name: ")
+
+if(name in l):
+    print("Your name is in the list")
+else:
+    print("Your name is not in the list")
+
+    ##Case sensitive
